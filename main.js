b693d07d16633acb6dd52836b5e72b6e791fc53f
Charlotte Warfel <charlottewarfel@gmail.com>
2019-01-04 17:00:33 -0800

    implement renderSinglePost() to allPostsContainer effectively rendering entire blog

--- main.js
+++ main.js
@@ -7,90 +7,77 @@ function BlogPost(id, title, author, createdOn, body, tags) {
     this.createdOn = createdOn;
     this.body = body;
     this.tags = tags;
-   
-}
-BlogPost.prototype.renderBlog= function() {
-
-
 }
 
 
-    BlogPost.prototype.renderSinglePost= function() {
-            this.renderTitle();
-            this.renderAuthor();
-            this.renderCreatedOn();
-            this.renderBody();
-            this.renderTags();
+BlogPost.prototype.renderSinglePost= function() {
+    var  allPostsContainer = document.getElementById('all-posts');
+    var singlePostDiv = document.createElement('div');
 
-}
+        this.renderTitle(singlePostDiv);
+        this.renderAuthor(singlePostDiv);
+        this.renderCreatedOn(singlePostDiv);
+        this.renderBody(singlePostDiv);
+        this.renderTags(singlePostDiv);
 
-BlogPost.prototype.renderTitle= function() {
-    var titleParent = document.getElementById('singlePost-container');
+            allPostsContainer.appendChild(singlePostDiv);
+
+        }
+
+BlogPost.prototype.renderTitle= function(parent) {
+
     var title = document.createElement('h2');
     title.textContent = this.title;
-
-    // console.log(title);
-    titleParent.appendChild(title);
+    parent.appendChild(title);
 }
 
-BlogPost.prototype.renderAuthor= function() {
-    var authorParent = document.getElementById('singlePost-container');
+BlogPost.prototype.renderAuthor= function(parent) {
+
     var author = document.createElement('div');
     author.textContent = this.author;
-
-    // console.log(author);
-    authorParent.appendChild(author);
+    parent.appendChild(author);
 }
 
-BlogPost.prototype.renderCreatedOn= function() {
-    var createdOnParent = document.getElementById('blog-creation');
+BlogPost.prototype.renderCreatedOn= function(parent) {
+    
     var createdOn = document.createElement('span');
     createdOn.textContent = this.createdOn;
-
-    // console.log(createdOn);
-    createdOnParent.appendChild(createdOn)
+    parent.appendChild(createdOn)
 
 }
 
-BlogPost.prototype.renderBody= function() {
-    var bodyParent = document.getElementById('blog-body');
+BlogPost.prototype.renderBody= function(parent) {
+    
     var body = document.createElement('p');
     body.textContent= this.body;
-
-    // console.log(body);
-    bodyParent.appendChild(body);
+    parent.appendChild(body);
 
 }
 
-BlogPost.prototype.renderTags=function() {
-    var tagsParent = document.getElementById('blog-tags');
+BlogPost.prototype.renderTags=function(parent) {
+    
     var tags = document.createElement('span');
     tags.textContent= this.tags;
-
-    // console.log(tags);
-    tagsParent.appendChild(tags);
+    parent.appendChild(tags);
 }
 
 
-    var allPosts = [];
-    data.forEach(function(element) {
+var allPosts = [];
+data.forEach(function(element) {
     var newBlog = new BlogPost(element.id, element.title, element.author, element.createdOn, element.body, element.tags);
     allPosts.push(newBlog);
-
-    // console.log(allPosts);
-    // console.log(data);
-
-
-//   take element run through constructor and turn into blogpost, 
-// add in to allposts variable
 })
 
+allPosts.forEach(function(post) {
+    post.renderSinglePost();
+
+}) 
 
 
 
 
-var singleBlog = new BlogPost(8, 'My Title','char', 'January 14, 2019', 'hello', 'tags');
- singleBlog.renderSinglePost();
+
+
 
 
 
